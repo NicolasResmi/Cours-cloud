59f8bb5801904949a60d696829c55bba5164f3e0
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-11-14 12:01:29 +0100

    mon petit commit 31

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
-<?php
+<!-- <?php
 
-define('MYSQL_USER', 'root');
-define('MYSQL_PASSWORD', 'root');
-define('MYSQL_DATABASE', 'tpPoo');
-define('MYSQL_HOST', 'localhost');
-define('MYSQL_PORT', 8889);
+// define('MYSQL_USER', 'root');
+// define('MYSQL_PASSWORD', 'root');
+// define('MYSQL_DATABASE', 'tpPoo');
+// define('MYSQL_HOST', 'localhost');
+// define('MYSQL_PORT', 8889);
 // define('MYSQL_SOCKET', '/tmp/mysql.sock');
-?>
+?> -->
 
 <?php
 // This assumes a fictional application with an array named $settings.
@@ -19,10 +19,10 @@ if ($relationships) {
 		foreach ($relationships['database'] as $endpoint) {
 			$settings['database_driver'] = 'pdo_' . $endpoint['scheme'];
 			$settings['database_host'] = $endpoint['host'];
-			$settings['database_name'] = $endpoint['path'];
-			$settings['database_port'] = $endpoint['port'];
-			$settings['database_user'] = $endpoint['username'];
-			$settings['database_password'] = $endpoint['password'];
+			$settings['database_name'] = 'php_poo';
+			$settings['database_port'] = 8889;
+			$settings['database_user'] = 'root';
+			$settings['database_password'] = '';
 			break;
 		}
 	}
