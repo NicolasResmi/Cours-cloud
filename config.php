80159ea3f21637abafb0be23fcb06949a411f637
NicolasResmi <nicolas.resmi@etu-webschoolfactory.fr>
2018-11-14 12:35:31 +0100

    mon petit commit 31

--- config.php
+++ config.php
@@ -1,32 +1,29 @@
-<!-- 
-
-// define('MYSQL_USER', 'root');
-// define('MYSQL_PASSWORD', 'root');
-// define('MYSQL_DATABASE', 'tpPoo');
-// define('MYSQL_HOST', 'localhost');
-// define('MYSQL_PORT', 8889);
-// define('MYSQL_SOCKET', '/tmp/mysql.sock');
- -->
+<?php
+define('MYSQL_USER', 'root');
+define('MYSQL_PASSWORD', 'root');
+define('MYSQL_DATABASE', 'php_poo');
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_PORT', 8889);
+define('MYSQL_SOCKET', '/tmp/mysql.sock');
+?>
 
 <?php
 // This assumes a fictional application with an array named $settings.
 $relationships = getenv('PLATFORM_RELATIONSHIPS');
-var_dump($relationships);
-die;
 if ($relationships) {
 	$relationships = json_decode(base64_decode($relationships), TRUE);
+
 	// For a relationship named 'database' referring to one endpoint.
 	if (!empty($relationships['database'])) {
 		foreach ($relationships['database'] as $endpoint) {
-			$settings['database_driver'] = 'pdo_' . $endpoint['scheme'];
-			$settings['database_host'] = '$endpoint['host']';
-			$settings['database_name'] = 'php_poo';
-			$settings['database_port'] = 8889;
-			$settings['database_user'] = 'root';
-			$settings['database_password'] = '';
+            $settings['database_driver'] = 'pdo_' . $endpoint['scheme'];
+			$settings['database_host'] = $endpoint['host'];
+			$settings['database_name'] = $endpoint['path'];
+			$settings['database_port'] = $endpoint['port'];
+			$settings['database_user'] = $endpoint['username'];
+			$settings['database_password'] = $endpoint['password'];
 			break;
 		}
 	}
 }
-
 ?>
